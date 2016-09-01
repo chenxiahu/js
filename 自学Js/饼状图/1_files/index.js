727f1ff121a34603f54166133e0fbda2e1184716
chenxiahu <1153654714@qq.com>
2016-09-01 10:32:25 +0800

    js各种操作

---
+++ 自学Js/饼状图/1_files/index.js
@@ -0,0 +1,32 @@
+/*
+     var data=[11964,18799,51966,35876,5466]
+     var e=["大专及以上","高中和中专","初中","小学","文盲人口"]
+	 var width=400,
+	 height=400;
+	 var svg=d3.select("#container")
+	 .append("svg")
+	 .attr("width",width)
+	 .attr("height",height);
+	 
+	 var g=svg.append("g")
+	 .attr("transform","translate(200,200)");
+	 
+	 var arc_generator=d3.svg.arc()
+	 .innerRadius(100)
+	 .outerRadius(200)
+	   var angle_data=d3.layout.pie()
+	   .value(function(d){return d});
+	   var color=d3.scale.category10();
+       g.selectAll("path")
+	  .data(angle_data(data))
+	  .enter()
+	  .append("path")
+	  .attr("d",arc_generator) 
+	  .style("fill",function(d,i){return color(i);}) 
+	  g.selectAll("text")
+	  .data(angle_data(e))
+	  .enter()
+	  .append("text")
+	  .text(function(e){return e;}) 
+	  
+	 */
